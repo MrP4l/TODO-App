18238cc154e71beafe9a01c237e56c3a1b41a8d4
MrP4l <101936971+MrP4l@users.noreply.github.com>
2023-08-12 19:01:50 +0200

    Adding responsiveness

---
+++ src/responsiveness.js
@@ -0,0 +1,58 @@
+const mediaQuery = window.matchMedia('(max-width: 425px)');
+
+function dropDownMenuButton() {
+    const button = document.getElementById("button");
+
+    if (mediaQuery.matches) {
+        if (!button) {
+            createButton();
+        }
+    } else {
+        const button = document.getElementById("button");
+        if (button) {
+            button.remove();
+        }
+    }
+}
+
+function createButton() {
+    const sideAndMainContainer = document.getElementById("sideAndMainContainer");
+    const button = document.createElement("button");
+    const arrow = document.createElement("i");
+
+    button.id = "button";
+    arrow.id = "arrow";
+
+    arrow.classList.add("bx");
+    arrow.classList.add("bx-chevron-down");
+
+    button.appendChild(arrow);
+    sideAndMainContainer.prepend(button);
+
+    return button;
+}
+
+function createDropDownMenu() {
+    dropDownMenuButton();
+
+    const button = document.getElementById("button");
+    const sideColumn = document.getElementById("sideColumn");
+    const toggleArrow = document.getElementById("arrow");
+
+    sideColumn.classList.add("sideColumn");
+    // TODO add the hidden to overflow-y on content container when the dropdown is toggled
+    const toggleDropdown = () => {
+        sideColumn.classList.toggle("show");
+        toggleArrow.classList.toggle("arrow");
+      };
+
+    button.addEventListener("click", (e) => {
+        e.stopPropagation();
+        toggleDropdown();
+    })
+
+    window.addEventListener('resize', dropDownMenuButton);
+}
+
+
+export default createDropDownMenu;
--- src/user_interface.js
+++ src/user_interface.js
@@ -1,9 +1,11 @@
 import userStaticInterface from "./user_static_interface";
 import userDynamicInterface from "./user_dynamic_interface";
+import createDropDownMenu from "./responsiveness";
 
 function userInterface() {
     userStaticInterface();
     userDynamicInterface();
+    createDropDownMenu();
 };
 
 export default userInterface;
